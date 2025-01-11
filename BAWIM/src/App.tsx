660847cf12fd5753be6b1cfe1e7adbbd49de9478
Patr0sss <kkiejno@gmail.com>
2025-01-11 15:29:25 +0100

    Basic routes structure and styling

--- BAWIM/src/App.tsx
+++ BAWIM/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/homePage/home";
+import Login from "./pages/loginPage/login";
+import Register from "./pages/registerPage/register";
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
-
-export default App;
---
+++ BAWIM/src/pages/loginPage/login.tsx
@@ -0,0 +1,9 @@
+import styles from "./login.module.css";
+
+export default function Login() {
+  return (
+    <div className={styles.base}>
+      <h1>Login</h1>
+    </div>
+  );
+}
