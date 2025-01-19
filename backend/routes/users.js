32b710c58ebd277cfaf98a30144dfef66b88e57b
JaSycz <kubasy2019@gmail.com>
2025-01-19 17:03:28 +0100

    added message system

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,10 +10,42 @@ router.get('/get-all', authorize, async function(req, res) {
      const users = await pool.query('SELECT user_id,username  FROM users WHERE $1!=user_id', [user_id]);
      res.status(200).json(users.rows);
    } catch (err) {
-     console.error(err);
+     
      res.status(500).json({ error: "Server error" });
    }
  });
 
+router.post('/get-messages', authorize, async function(req,res){
+  const user_one_id = req.user.id
+  const {user_two_id} = req.body
  
+  try {
+    const user_one_msg = await pool.query('SELECT * FROM messages Where (sender_id=$1 AND receiver_id=$2) OR (sender_id=$2 AND receiver_id=$1) ',[user_one_id,user_two_id]);
+    res.status(200).json(user_one_msg.rows);
+  } catch (err){
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.post('/post-messages', authorize, async function (req,res) {
+  const user_one_id = req.user.id 
+  const {user_two_id,message} = req.body
+  const today = new Date();
+  const options = { 
+    year: 'numeric', 
+    month: '2-digit', 
+    day: '2-digit', 
+    hour: '2-digit', 
+    minute: '2-digit' 
+  };
+  const formattedDate = today.toLocaleString('pl-PL', options);
+  console.log(formattedDate)
+  try {
+    await pool.query('INSERT INTO messages (sender_id,receiver_id, message,created_at) VALUES ($1,$2,$3,$4)', [user_one_id, user_two_id, message, formattedDate]);
+    res.status(200).json({msg: "message added"});
+  }
+  catch (err){
+    res.status(500).json({error: "Server error"});
+  }
+})
 module.exports = router;
--- database.sql
+++ database.sql
@@ -7,9 +7,21 @@ CREATE TABLE users(
     user_password VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username,user_password) VALUES (
-    'admin', 'ktd1332'
+
+CREATE TABLE messages(
+    sender_id INTEGER,
+    receiver_id INTEGER,
+    message TEXT,
+    created_at TEXT
 );
 
+INSERT INTO users (username,user_password) VALUES (
+    'admin', 'ktd1332'
+    
+);
 
+INSERT INTO users (username,user_password) VALUES (
+    'borygo', '123'
+    
+);
 SELECT * FROM users;
